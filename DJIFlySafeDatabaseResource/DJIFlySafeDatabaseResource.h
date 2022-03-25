fa67461f981ff01ecc93e6f1f829125496149114
Albin Törnqvist <albintornqvist@gmail.com>
2022-03-25 16:12:58 +0100

    Added a framework target

---
+++ DJIFlySafeDatabaseResource/DJIFlySafeDatabaseResource.h
@@ -0,0 +1,18 @@
+//
+//  DJIFlySafeDatabaseResource.h
+//  DJIFlySafeDatabaseResource
+//
+//  Created by Albin Törnqvist on 2022-03-25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DJIFlySafeDatabaseResource.
+FOUNDATION_EXPORT double DJIFlySafeDatabaseResourceVersionNumber;
+
+//! Project version string for DJIFlySafeDatabaseResource.
+FOUNDATION_EXPORT const unsigned char DJIFlySafeDatabaseResourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJIFlySafeDatabaseResource/PublicHeader.h>
+
+

e197e70e3131ca3d7f3ac121b9e7ba6a7f39de84
Albin Törnqvist <albintornqvist@gmail.com>
2022-03-25 16:04:53 +0100

    Recreated xcode project

---
+++ Shared/DJIFlySafeDatabaseResourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJIFlySafeDatabaseResourceApp.swift
+//  Shared
+//
+//  Created by Albin Törnqvist on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct DJIFlySafeDatabaseResourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
